7386a2ad65823d7781a117b3d1f563e64c9207f8
AnaBeloborodova <uroslovanasta@gmail.com>
2021-03-17 14:21:15 +0700

    :fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,6 @@
 function autoResize(iframe) {
     $(iframe).height($(iframe).contents().find('html').height());
+    console.log("function autoResize(iframe) {\n" +
+        "    $(iframe).height($(iframe).contents().find('html').height());};");
 };
 
